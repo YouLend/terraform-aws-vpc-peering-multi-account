b59d4e97277bdb1aa508b723f8ee0b3991b9e764
Aidan Fitzpatrick <aidan.fitzpatrick@youlend.com>
2021-01-11 14:04:52 +0000

    feat: test: testing

--- accepter.tf
+++ accepter.tf
@@ -80,14 +80,14 @@ data "aws_vpc" "accepter" {
 }
 
 # Lookup accepter subnets
-#data "aws_subnet_ids" "accepter" {
-#  provider = aws.accepter
-#  vpc_id   = local.accepter_vpc_id
-#}
+data "aws_subnet_ids" "accepter" {
+  provider = aws.accepter
+  vpc_id   = local.accepter_vpc_id
+}
 
 locals {
-#  accepter_subnet_ids       = distinct(sort(flatten(data.aws_subnet_ids.accepter.ids)))
-#  accepter_subnet_ids_count = length(local.accepter_subnet_ids)
+  accepter_subnet_ids       = distinct(sort(flatten(data.aws_subnet_ids.accepter.ids)))
+  accepter_subnet_ids_count = length(local.accepter_subnet_ids)
   accepter_vpc_id           = data.aws_vpc.accepter.id
   accepter_account_id       = data.aws_caller_identity.accepter.account_id
   accepter_region           = data.aws_region.accepter.name
